a8379310e377d62a806890bcdbc682283e360b7c
Stephanie Sison <sjsison27@icloud.com>
2022-08-16 20:40:23 -0700

    commit tree 38e04ed77d11120cca15043711607b7393846e9f

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,16 @@
 import React from "react";
+import user from "../data/user";
 
 function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{About() ? {user.bio} : null}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <links>
+        <h3>Links</h3>
+        <a href='{this.link.github}' text='{this.link.github}'></a>
+      </links>
     </div>
   );
 }
